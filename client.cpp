de03658763e7e2f3b613315146eb4638478d2531
Gabreu20 <48576715+Gabreu20@users.noreply.github.com>
2022-08-18 14:36:38 -0300

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,189 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+#include <iostream>
+    
+#define PORT     8080 
+#define MAXLINE 1024 
+    
+using namespace std;
+
+int num = 0;
+
+void receberInfo(int n, int sockfd, struct sockaddr_in     servaddr, char buffer[MAXLINE], unsigned int len);
+
+void enviar1Pacote10M(int sockfd, const char *message, struct sockaddr_in     servaddr,  char buffer[MAXLINE], unsigned int len, int n){
+    char letras[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    string msg;
+    int r;
+    for(int i = 0; i < 10240; i++){
+        r = rand() % 27;
+        msg = msg + letras[r];
+    }
+
+    //Divide o Arquivo > 1024 Bytes em Pacotes de No máximo 1024 Bytes
+    string pacotes[10];
+    for(int i = 0; i < 10; i++){
+        for(int j = 1024 * i; j < 1024 * (i + 1); j++){
+            pacotes[i] = pacotes[i] + msg[j];
+        }
+    }
+
+    for(int i = 0; i < 10; i++){
+        message = pacotes[i].c_str();
+        sendto(sockfd, (const char *)message, strlen(message), 
+                MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
+                    sizeof(servaddr));
+        receberInfo(n, sockfd, servaddr, buffer, len);
+    }
+}
+
+void enviar1PacoteGrande(int sockfd, const char *message, struct sockaddr_in     servaddr,  char buffer[MAXLINE], unsigned int len, int n){
+    char letras[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    string msg;
+    int r;
+    for(int i = 0; i < 1024; i++){
+        r = rand() % 27;
+        msg = msg + letras[r];
+    }
+
+    message = msg.c_str();
+    sendto(sockfd, (const char *)message, strlen(message), 
+            MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
+                sizeof(servaddr));
+    receberInfo(n, sockfd, servaddr, buffer, len);
+    
+}
+
+void enviarXPacotes(int sockfd, const char *message, struct sockaddr_in     servaddr, int numPackages,  char buffer[MAXLINE], unsigned int len, int n){
+
+    while(numPackages > 0){
+        string msg = "PACOTE " + to_string(num);
+        message = msg.c_str();
+        sendto(sockfd, (const char *)message, strlen(message), 
+                MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
+                    sizeof(servaddr));
+        receberInfo(n, sockfd, servaddr, buffer, len);
+        num++;
+        numPackages--;    
+    }
+}
+
+void connect(int sockfd, const char *message, struct sockaddr_in     servaddr,  char buffer[MAXLINE], unsigned int len, int n){
+    string msg = "Client Conectou";
+    message = msg.c_str();
+    sendto(sockfd, (const char *)message, strlen(message), 
+                MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
+                    sizeof(servaddr));
+}
+
+void disconnect(int sockfd, const char *message, struct sockaddr_in     servaddr,  char buffer[MAXLINE], unsigned int len, int n){
+    string msg = "Client Desconectou";
+    message = msg.c_str();
+    sendto(sockfd, (const char *)message, strlen(message), 
+                MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
+                    sizeof(servaddr));
+}
+
+void receberInfo(int n, int sockfd, struct sockaddr_in     servaddr, char buffer[MAXLINE], unsigned int len){
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+                MSG_WAITALL, (struct sockaddr *) &servaddr, 
+                &len); 
+    buffer[n] = '\0';
+    if(n > 0) 
+        printf("Server : %s\n", buffer); 
+}
+
+void menu(int n, char buffer[MAXLINE], unsigned int len, char const *message, int sockfd, struct sockaddr_in     servaddr){
+    int escolha = -1;
+    while(escolha < 1){
+        cout << "===============================================" << endl << endl << endl;
+        cout << "Escolha uma opção:" << endl;
+        cout << "[1]Enviar 1 pacote para o Servidor" << endl;
+        cout << "[2]Enviar 10 pacotes para o Servidor" << endl;
+        cout << "[3]Enviar 1 pacote de 1M(1024 Bytes) para o Servidor" << endl;
+        cout << "[4]Enviar 1 pacote de 10M(10240 Bytes) para o Servidor" << endl;
+        cout << "[5]Desconectar" << endl;
+        cin >> escolha;
+        if(escolha < 1 || escolha > 5){
+            cout << "OPÇÃO INVÁLIDA!" << endl;
+            menu(n, buffer, len, message, sockfd, servaddr);
+        }
+
+    }
+    switch(escolha){
+        case 1:
+        {
+            num++;
+            string msg = "PACOTE " + to_string(num);
+            message = msg.c_str();
+
+            sendto(sockfd, (const char *)message, strlen(message), 
+                MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
+                    sizeof(servaddr)); 
+            printf("enviado.\n"); 
+            receberInfo(n, sockfd, servaddr, buffer, len);
+            menu(n, buffer, len, message, sockfd, servaddr);
+            break;
+        }
+        case 2:
+        {
+            enviarXPacotes(sockfd, message, servaddr, 10, buffer, len, n);
+            menu(n, buffer, len, message, sockfd, servaddr);
+            break;
+        }
+        case 3:
+        {
+            enviar1PacoteGrande(sockfd, message, servaddr, buffer, len, n);
+            menu(n, buffer, len, message, sockfd, servaddr);
+            break;
+        }
+        case 4:
+        {
+            enviar1Pacote10M(sockfd, message, servaddr, buffer, len, n);
+            menu(n, buffer, len, message, sockfd, servaddr);
+            break;
+        }
+        case 5:
+        {
+            disconnect(sockfd, message, servaddr, buffer, len, n);
+            exit(0);
+        }
+    }
+}
+
+// Driver code 
+int main() { 
+    int sockfd; 
+    char buffer[MAXLINE]; 
+    char const *message = ""; 
+    struct sockaddr_in     servaddr; 
+
+    // Creating socket file descriptor 
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    
+    memset(&servaddr, 0, sizeof(servaddr)); 
+        
+    // Filling server information 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_port = htons(PORT); 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+        
+    int n; 
+    unsigned int len;
+        
+    connect(sockfd, message, servaddr, buffer, len, n);
+
+    menu(n, buffer, len, message, sockfd, servaddr);
+    
+    close(sockfd); 
+    return 0; 
+}
---
+++ server.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <string> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+    
+#define PORT     8080 
+#define JANELA 1024 * 10
+    
+using namespace std;
+
+int main() { 
+    srand(time(NULL));
+    int sockfd; 
+    char buffer[JANELA]; 
+    int num_seq = 0;
+
+    int janela = 10;
+
+    int acks = 0;
+    int ackSeq = 0;
+
+    char const *ack = "ACK"; 
+    struct sockaddr_in servaddr, cliaddr; 
+        
+    // Creating socket file descriptor 
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+        
+    memset(&servaddr, 0, sizeof(servaddr)); 
+    memset(&cliaddr, 0, sizeof(cliaddr)); 
+        
+    // Filling server information 
+    servaddr.sin_family    = AF_INET; // IPv4 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+    servaddr.sin_port = htons(PORT); 
+        
+    // Bind the socket with the server address 
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
+            sizeof(servaddr)) < 0 ) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+        
+    int n; //tamanho do em bytes do pacote recebido
+    unsigned int len;
+    
+    len = sizeof(cliaddr);   
+
+    //RECEBE INFORMAÇÃO QUE O CLIENT CONECTOU-SE
+    n = recvfrom(sockfd, (char *)buffer, JANELA,  
+                    MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+                    &len);
+    printf("%s\n", buffer); 
+
+    while(true){
+        n = recvfrom(sockfd, (char *)buffer, JANELA,  
+                    MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+                    &len); 
+        //==========================Perdas Arbitrárias de Pacotes==========================
+        //Dessa forma, cerca de 3/4 dos pacotes serão recebidos e 1/4 será "Perdido"
+        //No relatoŕio, Os números sem perdas serão apresentados com testes feitos sem essa implementação de perdas de pacotes
+        int R = rand()%4;
+        if(R < 3){
+            acks++;
+            ackSeq = ackSeq + n;
+            buffer[n] = '\0'; 
+            printf("Client : %s  \nNum. de Sequencia: %i\n", buffer, num_seq); 
+            num_seq = num_seq + n;
+        }
+        else{
+            //PERDEU O PACOTE
+        }
+        //==========================Perdas Arbitrárias de Pacotes==========================
+
+        if(acks%janela == 0 && acks > 0){
+            string send_ack = "ACK " + to_string(ackSeq);
+
+            ack = send_ack.c_str();
+
+            sendto(sockfd, (const char *)ack, strlen(ack),  
+            MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
+                len); 
+            printf("ACK SENT\n"); 
+        }
+        else{
+            const char *empty = "";
+            sendto(sockfd, (const char *)empty, strlen(empty),  
+            MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
+                len);
+        }
+    }
+
+    return 0; 
+}
